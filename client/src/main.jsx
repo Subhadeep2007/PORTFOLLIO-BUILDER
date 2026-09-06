0173cb5c9c4b93acb06501d48c71db458c247fc8
SubhadeepGarai2006 <subhadeep777sayan@gmail.com>
2026-09-06 13:34:03 +0530

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-
-import "./styles/tailwind.css";
+import "./index.css";
 
 
 ReactDOM.createRoot(
